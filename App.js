6d3c8a71bee89873713078898b89efc92f93b610
ermaneeshtiwari <er.maneeshtiwari@gmail.com>
2023-07-18 23:07:38 +0530

    Episode -3

--- App.js
+++ App.js
@@ -1,41 +1,27 @@
-// const heading = document.createElement('h1');
-// heading.innerHTML = "Hello Word form javascript";
-// const root = document.getElementById('root');
-// root.appendChild(heading);
-
-// How to add h1 in react 
-
-// const heading = React.createElement('h1',{id:"heading", className:"heading"},'Hello I am here on React');
-// console.log(heading);
-// const root = ReactDOM.createRoot(document.getElementById('root'));
-// root.render(heading);
-
-// How to add nested div in react 
-
-{/* <div id="parent">
-    <div id="child 1">
-        <h1>This 1 child h1</h1>
-        <h2>This 1 child h2</h2>
-    </div>
-    <div id="child 2">
-        <h1>This 2 child h1</h1>
-        <h2>This 2 child h2</h2>
-    </div>
-</div> */}
 import React from "react";
 import ReactDOM from "react-dom";
 
-const parent = React.createElement('div', {id:"parent"} ,[
-    React.createElement('div', {id:"child1"}, [
-        React.createElement('h1', {}, 'This 1 child h1'), 
-        React.createElement('h2', {}, 'This 1 child h2')
-    ]),
-    React.createElement('div', {id:"child2"}, [
-        React.createElement('h1', {}, 'This 2 child h1'),
-        React.createElement('h2', {}, 'This 2 child h2')
-    ])
-]);
+// const parent = React.createElement('div', {id:"parent"} ,[
+//     React.createElement('div', {id:"child1"}, [
+//         React.createElement('h1', {}, 'This 1 child h1'), 
+//         React.createElement('h2', {}, 'This 1 child h2')
+//     ]),
+//     React.createElement('div', {id:"child2"}, [
+//         React.createElement('h1', {}, 'This 2 child h1'),
+//         React.createElement('h2', {}, 'This 2 child h2')
+//     ])
+// ]);
+// const parent = (<h1 id='heading'>
+//     This is parent
+//         </h1>);
+const TitleComponent = () => {
+    return <h1>This is Title</h1>;
+}
 
-console.log(parent);
+const HeadingComponet = () => (<div id="container">
+    <TitleComponent />
+    <div className="heading">This is Heading compnent</div>
+    </div>);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(parent);
+root.render(<HeadingComponet />);
