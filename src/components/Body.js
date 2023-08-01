c102ef2d205703bb9e98345f00cc364d70ad19e4
ermaneeshtiwari <er.maneeshtiwari@gmail.com>
2023-08-01 20:54:15 +0530

    Episode -5

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,12 +1,30 @@
 import RestorentCart from "./RestorentCart";
 import Search from "./Search";
 import {restoList} from "../utils/mockData";
+import { useState } from "react";
 const Body =() => {
+    const [listofrestoList, setListofrestorList] = useState(restoList);
+
+    
+   
     return(
         <div className="restor-container">
-            <div className="searchform"><Search /></div>
+            <div className="searchform">
+                <div className="search-bar">
+                    <Search />
+                    <div className="filter-btn">
+                    <button className="filter-btn-round" onClick={()=>{
+                        const filteredList = listofrestoList.filter (
+                            (res) => res.info.avgRating > 4
+                        );
+                        setListofrestorList(filteredList);
+                    }}>Top Rates Restorant</button>
+                    </div>
+                </div>
+            </div>
+            
             <div className="resto-list">
-                {restoList.map((restorant)=>
+                {listofrestoList.map((restorant)=>
                     <RestorentCart key={restorant.info.id} resData={restorant} />
                 )}
             </div>
